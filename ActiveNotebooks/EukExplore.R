68d38d65558913c58694526cd5436f385f79cde0
Jacob A Cram <jcram@umces.edu>
2022-12-19 15:30:33 -0500

    Sulfide plot

--- ActiveNotebooks/EukExplore.R
+++ ActiveNotebooks/EukExplore.R
@@ -2,6 +2,12 @@ library(here)
 library(tidyverse)
 library(ggh4x)
 
+#  [1] "Annelida"            "Arthropoda"          "Ascomycota"          "Basidiomycota"       "Bicosoecida"         "Brachiopoda"        
+#  [7] "Cercozoa"            "Chlorophyta_ph"      "Choanoflagellida"    "Chytridiomycota"     "Ciliophora"          "Cnidaria"           
+# [13] "Cryptomonadales"     "Ctenophora"          "Dinoflagellata"      "Florideophycidae"    "Ichthyosporea"       "Incertae_Sedis"     
+# [19] "Kathablepharidae"    "Labyrinthulomycetes" "MAST-12"             "Mollusca"            "Nematoda"            "Ochrophyta"         
+# [25] "Peronosporomycetes"  "Phragmoplastophyta"  "Picozoa" 
+
 # Bring in data
 #source(here("RScripts", "InitialProcessing_3.R"))
 load(here::here("RDataFiles", "InitialProcessing_3.RData"))
@@ -10,10 +16,33 @@ source(here("RLibraries", "Brigandine_Library.R"))
 ## Eukaryotes
 ches_brigandine(Phylum, Kingdom, taxa01 %>% filter(Kingdom == "Eukaryota") %>% pull(Kingdom) %>% unique(), min = 4, max = 7, thresh = 10^5, ns = nonSpikes)
 #art <- ches_brigandine(, Class, taxa01 %>% filter(Phylum == "Arthropoda") %>% pull(Class) %>% unique(), min = 4, thresh = 0, ns = nonSpikes)
+
+
+## Arthropods
 art1 <- ches_brigandine(Order, Class, taxa01 %>%  pull(Class) %>% unique() %>% na.omit(), thresh = 10^5, min = 3, max = 5,
                         ns = nonSpikes %>% filter(Phylum == "Arthropoda"))
 art1
-art2 <- ches_brigandine_L2(Tag_ASV, Order, taxa01 %>%  pull(Order) %>% unique() %>% na.omit(), thresh = 10^4, min = 3, max = 5,
+art2 <- ches_brigandine_L2(Tag_ASV, Order, taxa01 %>%  pull(Order) %>% unique() %>% na.omit(), thresh = 10^4, min = 0, max = 4,
                         ns = nonSpikes %>% filter(Phylum == "Arthropoda"))
 art2 + facet_nested(Class + Order ~ Depth, drop = TRUE, scales = "free", space = "free") +
   labs(y = "ASV")
+
+## Other stuff
+cil <- ches_brigandine_L2(Tag_ASV, Order, taxa01 %>%  pull(Order) %>% unique() %>% na.omit(), thresh = 10^4, min = 3, max = 5,
+                        ns = nonSpikes %>% filter(Phylum == "Ciliophora"))
+cil + facet_nested(Class + Order ~ Depth, drop = TRUE, scales = "free", space = "free") +
+  labs(y = "ASV")
+
+## Plot euk phylum
+plot_euk_phylum <- function(loc_phylum){
+plt <- ches_brigandine_L2(Tag_ASV, Order, taxa01 %>%  pull(Order) %>% unique() %>% na.omit(), thresh = 10^4, min = 0, max = 4,
+                        ns = nonSpikes %>% filter(Phylum == loc_phylum))
+plt + facet_nested(Class + Order ~ Depth, drop = TRUE, scales = "free", space = "free") +
+  labs(y = "ASV")
+}
+
+plot_euk_phylum("Mollusca")
+
+plot_euk_phylum("Ctenophora")
+
+plot_euk_phylum("Annelida")
--- RScripts/Brigandine_2.R
+++ RScripts/Brigandine_2.R
@@ -3,29 +3,40 @@
 
 library(here)
 library(tidyverse)
+library(cowplot)
 
 # Bring in data
 #source(here("RScripts", "InitialProcessing_3.R"))
 load(here::here("RDataFiles", "InitialProcessing_3.RData"))
 source(here("RLibraries", "Brigandine_Library.R"))
 
+nonSpikes$Depth <- ordered(nonSpikes$Depth, levels = c("Surface", "Oxycline", "Bottom"))
+
 # Phylum Level Plots
-phylum_mg_plot <- ches_brigandine(Phylum, Kingdom, taxa01 %>% pull(Kingdom) %>% unique(), min = 4, max = 7, thresh = 10^6, ns = nonSpikes)
+phylum_mg_plot <- ches_brigandine(Phylum, Kingdom, taxa01 %>% pull(Kingdom) %>% unique(), min = 4, max = 7, thresh = 10^6, ns = nonSpikes %>%
+                                  # filter(Kingdom != "Eukaryota") %>%
+                                    identity()
+                                  )
 phylum_L_plot <- ches_brigandine_L(Phylum, Kingdom, taxa01 %>% pull(Kingdom) %>% unique(), min = 4, max = 9, thresh = 10^6, ns = nonSpikes)
 
 ggsave(here("Figures", "Phylum_per_mg_brig.png"), height = 5.5, width = 6.5, plot = phylum_mg_plot)
 ggsave(here("Figures", "Phylum_per_L_brig.png"), height = 5.5, width = 6.5, plot = phylum_L_plot)
 
 ## Planktomycetes ASVs
-plankto_mg_plot <- ches_brigandine(Tag_ASV, Order, taxa01 %>% filter(Phylum == "Planctomycetes") %>% pull(Order) %>% unique() %>% na.omit(), thresh = 10^6, min = 3)
-plankto_L_plot <- ches_brigandine_L(Tag_ASV, Order, taxa01 %>% filter(Phylum == "Planctomycetes") %>% pull(Order) %>% unique() %>% na.omit(), thresh = 10^6, min = 3)
+plankto_mg_plot <- ches_brigandine(Tag_ASV, Order, taxa01 %>% filter(Phylum == "Planctomycetes") %>% pull(Order) %>% unique() %>% na.omit(), thresh = 10^6, min = 3) +
+  guides(size = "none", legend.position = "top") + labs(y = "Tag; ASV")
+plankto_L_plot <- ches_brigandine_L(Tag_ASV, Order, taxa01 %>% filter(Phylum == "Planctomycetes") %>% pull(Order) %>% unique() %>% na.omit(), thresh = 10^6, min = 3) + 
+   labs(y = "Tag; ASV")
 
 ggsave(here("Figures", "Plankto_per_mg_brig.png"), height = 4.5, width = 7.5, plot = plankto_mg_plot)
 ggsave(here("Figures", "Plankto_per_L_brig.png"), height = 4.5, width = 7.5, plot = plankto_L_plot)
 
+plankto_both <- plot_grid(plankto_mg_plot, plankto_L_plot, ncol = 1, labels = LETTERS)
+ggsave(here("Figures", "Plankto_combined.svg"), height = 9, width = 7.5, plot = plankto_both)
 
 ## Snow dwellers primarely live on large particles
-mainHabitats <- nonSpikes %>% # was ns20
+mainHabitats <- nonSpikes20 %>% # was ns20
+  filter(Phylum != "Eukaryota") %>%
   group_by(Tag_ASV, Size_Class) %>% 
   summarise(copiesPerL = mean(copiesPerL)) %>%
   ungroup() %>% group_by(Tag_ASV) %>%
@@ -40,20 +51,21 @@ snowDwellers <- mainHabitats %>%
   filter(Size_Class >= 20) %>%
   pull(Tag_ASV)
 
-snow_dwellers_plot <- ches_brigandine_L(ASV, Phylum,
+snow_dwellers_plot <- ches_brigandine_L2(ASV, Phylum,
                 taxa %>% filter(Tag_ASV %in% snowDwellers) %>% pull(Phylum) %>% unique() %>% na.omit,
                 min = 3, max = 7, 
                 ns = nonSpikes20 %>% rename(ASV0 = ASV, ASV = Tag_ASV) %>% 
                   filter(ASV %in% snowDwellers) %>%
                   #mutate(ASV = recode(ASV, `Arachnida;9` = "Acartia;9")), # If you blast the "arachnid" sequence, it turns out to be Acartia tonsa
                   mutate(ASV = str_replace(ASV, "Arachnida", "Acartia")), # more general solution than the above line
-                thresh = 10^4)
+                thresh = 10^4) +
+  facet_nested(Phylum ~ Depth, drop = TRUE, scales = "free", space = "free")
 
-snow_dwellers_plot <- ches_brigandine_L(ASV, Phylum,
-                taxa %>% filter(Tag_ASV %in% snowDwellers) %>% pull(Phylum) %>% unique() %>% na.omit,
-                min = 3, max = 7, 
-                 # more general solution than the above line
-                thresh = 10^3, ns = nonSpikes  %>% filter(Tag_ASV %in% snowDwellers))
+# snow_dwellers_plot <- ches_brigandine_L(ASV, Phylum,
+#                 taxa %>% filter(Tag_ASV %in% snowDwellers) %>% pull(Phylum) %>% unique() %>% na.omit,
+#                 min = 3, max = 7, 
+#                  # more general solution than the above line
+#                 thresh = 10^3, ns = nonSpikes  %>% filter(Tag_ASV %in% snowDwellers))
 
 ggsave(here("Figures", "SnowDwellers_per_L_brig.png"), height = 4.5, width = 7.5, plot = snow_dwellers_plot)
-ggsave(here("Figures", "SnowDwellers_per_L_brig_narrow.png"), height = 4.5, width = 5.5, plot = snow_dwellers_plot)
+ggsave(here("Figures", "SnowDwellers_per_L_brig_narrow.png"), height = 4.5, width = 6, plot = snow_dwellers_plot)
